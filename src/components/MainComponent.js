31860a5c3ebc35a29d1bc31e43fa734f81942f5a
Irina Zhyzhyian <gygyjan.iryna@gmail.com>
2020-11-15 22:23:19 +0200

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -23,7 +25,9 @@ class Main extends Component {
       promotions: PROMOTIONS,
       leaders: LEADERS
     };
+    
   }
+  
 
   render() {
 
@@ -36,12 +40,22 @@ class Main extends Component {
           />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div>
         <Header />
            <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
             </Switch>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
-
-    function RenderMenuItem ({dish, onClick}) {
-        return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-            </Card>
-        );
-    }
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     const Menu = (props) => {
 
@@ -23,8 +13,31 @@ import { Card, CardImg, CardImgOverlay,
             );
         });
 
+        function RenderMenuItem ({dish, onClick}) {
+            return (
+                <Card>
+                    <Link to={`/menu/${dish.id}`} >
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Link>
+                </Card>
+            );
+        }
+
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div className="row">
                     {menu}
                 </div>
